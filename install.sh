2d15944850c0c9023a962980167894c5c44abff7
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2024-05-18 00:07:34 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
 
-# 引入依赖脚本
+#Introduce dependency scripts
 source ./scripts/lib.sh
 
-# 信号处理
+# Signal processing
 trap 'rm -f .env; exit' SIGINT SIGTSTP SIGTERM
 
-# 清理不需要的文件
+# Clean up unnecessary files
 clean_files
 
-# 安装依赖
-echo -e "\e[34m========= Checking server environment... | 检查服务器环境... =========\e[0m"
+# Install dependencies
+echo -e "\e[34m========== Checking server environment... | Checking server environment... =========\e[0m"
 install_dependencies
 
-# 检查环境
-echo -e "\e[34m========= Checking the panel environment... | 检查面板运行环境... =========\e[0m"
+# Check environment
+echo -e "\e[34m========== Checking the panel environment... | Checking the panel operating environment... =========\e[0m"
 check_env
 
-# 设置权限
-echo -e "\e[34m========= Setting Folder Permissions... | 设置文件夹权限... =========\e[0m"
+# Setting permissions
+echo -e "\e[34m========== Setting Folder Permissions... | Set folder permissions... =========\e[0m"
 set_permissions
 
-# 检查Composer
-echo -e "\e[34m========= Checking Composer... | 检查Composer... =========\e[0m"
+# Check Composer
+echo -e "\e[34m========= Checking Composer... | Checking Composer... =========\e[0m"
 check_composer
 
-# 执行Composer安装
-echo -e "\e[34m========= Installing packages via Composer... | 通过Composer安装程序包... =========\e[0m"
+# Execute Composer installation
+echo -e "\e[34m========== Installing packages via Composer... | Installing packages via Composer... =========\e[0m"
 composer install --no-interaction --no-dev --optimize-autoloader
 
-# 执行Panel安装
+# Execute Panel installation
 php artisan panel:install
 
-# 设置定时任务
-echo -e "\e[34m========= Enabling Panel schedule tasks... | 开启面板定时任务... =========\e[0m"
+# Set up scheduled tasks
+echo -e "\e[34m========== Enabling Panel schedule tasks... | Enabling panel scheduled tasks... =========\e[0m"
 set_schedule
 
-# 设置Horizon
-echo -e "\e[34m========= Setting Horizon daemon... | 设置Horizon守护程序... =========\e[0m"
+# Set Horizon
+echo -e "\e[34m========= Setting Horizon daemon... | Setting Horizon daemon... =========\e[0m"
 set_horizon
 
-# 下载IP数据库文件
-echo -e "\e[34m========= Downloading IP database files... | 下载IP数据库文件... =========\e[0m"
+# Download IP database file
+echo -e "\e[34m========== Downloading IP database files... | Downloading IP database files... =========\e[0m"
 cd scripts/ && bash download_dbs.sh && cd ../
